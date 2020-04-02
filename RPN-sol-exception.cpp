6339bcc77ec24b3bbd9912996b8f27d4b7052cce
samuel-shard <59847990+samuel-shard@users.noreply.github.com>
2020-04-02 15:36:00 -0400

    Part 2 Submitted

--- RPN-sol-exception.cpp
+++ RPN-sol-exception.cpp
@@ -1,4 +1,5 @@
-// RPN calculator
+// RPN Calculator Exception Handling 
+// Samuel Shard, 100745640
 
 #include <iostream> 
 #include <string>
@@ -8,83 +9,86 @@ using namespace std;
 bool is_str_digit(string);  // returns true if input parameter string is and integer (e.g., "127343")
 bool is_str_operator(string); // returns true if the input parameter is an operator character (e.g., "+", "*")
 
-
 int main() 
-{
+{	
+	//for inside switch statement
+	int value1, value2, value3;
+
     string in;
-    stack<string> st;
+    stack<string> st;   // stack definition
 
     cin >> in;
+	try{
+		while(in != "q"){
 
-    try 
-    {
-        while(in != "q")
-        {
-            if(is_str_digit(in))
-            {
-                st.push(in);
-            }
-            else if(is_str_operator(in))
-            {   
-                
-                if (st.size() < 2)
-                    throw 2;
+			if(is_str_digit(in)){
+				// if digit it input, push it onto the stack
+				st.push(in);
+			}
 
-                int value1 = stoi(st.top());
-                st.pop();
+			else if(is_str_operator(in)){
+				// throw error if an operator is called and stack has less than 2 digits on it
+				if(st.size() < 2){
+					throw 2;
+				}	
+				
+				value1 = stoi(st.top());
+				st.pop();
+				value2 = stoi(st.top());
+				st.pop();
 
-                int value2 = stoi(st.top());
-                st.pop();
+				switch(in[0]){
+					case '+':
+					//if plus operator, convert string values to integers and push sum to stack
+						value3 = value2 + value1;
+						break;
 
-                int value3 ;
+					case '-':
+					//if minus operator, subtract and push to stack
+						value3 = value2 - value1;
+						break;
 
-                switch(in[0])
-                {
-                    case '+':
-                        value3 = value2 + value1;
-                        break;
+					case '*':
+					//if multiply operator, multiply and push to stack
+						value3 = value2 * value1;
+						break;
 
-                    case '-':
-                        value3 = value2 - value1;
-                        break;
+					case '/':
+					//if divide operator, divide and push to stack
+						value3 = value2 / value1;
+						break;
+				}
+				st.push(to_string(value3));
+			}//end of else if
+			
+			//if not digit or operator, throw error 3
+			else{
+				throw 3;
+			}
 
-                    case '*':
-                        value3 = value2 * value1;
-                        break;
+			cin >> in;
+		}//end of while loop
+		if(st.size()!=1){
+			//After were  done, there should only be 1 answer on stack
+			throw 1;
+		}
+		cout << "The answer is : " << st.top() << endl;
 
-                    case '/':
-                        value3 = value2 / value1;
-                        break;
+	}//end of try block
+	catch(int e){
+		switch(e){
+			case 1:
+				cout << "There should only be 1 answer left on stack.\n";
+				return 1;
+			case 2:
+				cout <<"There are not 2 operands to perform operation on.\n";
+				return 1;
+			case 3:
+				cout <<"Unexpected input. Please enter +,-, *, /, or an integer.\n";
+				return 1;
+		}
 
-                }
-                st.push(to_string(value3));       
-            }
-            else
-                throw 3;
-
-            cin >> in;
-        }
-        if (st.size() != 1)
-            throw 3;
-        cout << "The answer is : " << st.top() << endl;
-
-    }
-    catch(int e)
-    {
-        switch(e){
-            case 1: 
-                cout << "The token is not operator nor integer !" << endl;
-                break;
-            case 2: 
-                cout << "There are not enough operand!" << endl;
-                break;
-            case 3:
-                cout << "The are extra operand in the stack! " << endl;
-                break;
-
-        }
-    }
-    
+	}
     return 0; 
 }
 
@@ -99,5 +103,5 @@ bool is_str_digit(string s)
 }
 bool is_str_operator(string s)
 {
-    return (s.length()==1 && (s[0] == '+' || s[0] == '-' || s[0] == '*' || s[0] == '/'));
+    return (s.length()==1 && (s[0] == '+' || s[0] == '-' || s[0] == '*' || s[0]== '/'));
 }
