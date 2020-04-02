43853561ee8dbe34fa57be7db5f05ed70dc88e23
samuel-shard <59847990+samuel-shard@users.noreply.github.com>
2020-04-02 15:06:17 -0400

    Part 1 Solution Uploaded

--- RPN.cpp
+++ RPN.cpp
@@ -8,9 +8,12 @@ using namespace std;
 bool is_str_digit(string);  // returns true if input parameter string is and integer (e.g., "127343")
 bool is_str_operator(string); // returns true if the input parameter is an operator character (e.g., "+", "*")
 
-
 int main() 
-{
+{	
+	//for inside switch statement
+	int value1, value2, value3;
+	string output;
+
     string in;
     stack<string> st;   // stack definition
 
@@ -19,28 +22,61 @@ int main()
     while(in != "q")
     {
         if(is_str_digit(in)){
-            // add your code here
+            // if digit it input, push it onto the stack
+			st.push(in);
         }
         else if(is_str_operator(in))
         {
-            // add your code here 
-
+            // throw error if an operator is called and stack has less than 2 digits on it
+			if(st.size() < 2){
+				cerr << "Stack size is less than 2."
+				 << " Stack size is: " << st.size() << endl;
+				 return 1;
+			}
             switch(in[0])
             {
                 case '+':
+				//if plus operator, convert string values to integers and push sum to stack
+					value1 = stoi(st.top());
+					st.pop();
+					value2 = stoi(st.top());
+					st.pop();
                     value3 = value2 + value1;
+					output = to_string(value3);
+					st.push(output);
                     break;
 
                 case '-':
+				//if minus operator, subtract and push to stack
+					value1 = stoi(st.top());
+					st.pop();
+					value2 = stoi(st.top());
+					st.pop();
                     value3 = value2 - value1;
+					output = to_string(value3);
+					st.push(output);
                     break;
 
                 case '*':
+				//if multiply operator, multiply and push to stack
+					value1 = stoi(st.top());
+					st.pop();
+					value2 = stoi(st.top());
+					st.pop();
                     value3 = value2 * value1;
+					output = to_string(value3);
+					st.push(output);
                     break;
 
                 case '/':
+				//if divide operator, divide and push to stack
+					value1 = stoi(st.top());
+					st.pop();
+					value2 = stoi(st.top());
+					st.pop();
                     value3 = value2 / value1;
+					output = to_string(value3);
+					st.push(output);
                     break;
 
             }
@@ -50,7 +86,6 @@ int main()
         }
         cin >> in;
     }
-
     cout << "The answer is : " << st.top() << endl;
 
     return 0; 
